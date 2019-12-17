cea29ed5b1cf063d94855cfa7ab63b7525db7647
Allen <allenmcndounger@gmail.com>
2019-12-17 15:41:43 +0300

    onSubmitting to the API

--- src/components/postForm.js
+++ src/components/postForm.js
@@ -9,17 +9,35 @@ export default class postForm extends Component {
     };
 
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmitPost.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmitPost(e) {
+    e.preventDefault();
+    const bodyForm = {
+      title: this.state.title,
+      body: this.state.body
+    };
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(bodyForm)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
   }
   render() {
     return (
       <div>
         <h3>Add Form</h3>
         <div>
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div>
               <label>Title:</label>
               <br />
@@ -41,7 +59,7 @@ export default class postForm extends Component {
               />
             </div>
             <div>
-              <button type="submit" />
+              <button type="submit">Submit</button>
             </div>
           </form>
         </div>
