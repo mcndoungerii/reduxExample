e9d9b59a03b89447dd526382e6992d4716ee479a
Allen <allenmcndounger@gmail.com>
2019-12-17 17:25:51 +0300

    mapStateToProps configured to our components through using rootReducer and passing it to our connect method

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,14 +1,12 @@
 import { FETCH_POSTS, NEW_POST } from "./types";
 
-export function fetchPosts() {
-  return function(dispatch) {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data =>
-        dispatch({
-          type: FETCH_POSTS,
-          payload: data
-        })
-      );
-  };
-}
+export const fetchPosts = () => dispatch => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data =>
+      dispatch({
+        type: FETCH_POSTS,
+        payload: data
+      })
+    );
+};
--- src/components/post.js
+++ src/components/post.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
-export default class Post extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    };
+class Post extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -23,3 +22,9 @@ export default class Post extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Post);
