5a6428ae0c0abf729194eb416d67e54b5a2e4853
Allen <allenmcndounger@gmail.com>
2019-12-17 16:41:09 +0300

    Configuring Redux

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 import Post from "./components/post";
 import PostForm from "./components/postForm";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr />
-      <Post />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm />
+        <hr />
+        <Post />
+      </div>
+    </Provider>
   );
 }
 
